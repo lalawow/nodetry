26f6c32253fd054acf0976aef6e476faa2c5b4c2
lalawow <lalawow@gmail.com>
2016-01-05 19:46:42 +0800

    insert css into jade

--- stocktry/public/javascripts/queryall.js
+++ stocktry/public/javascripts/queryall.js
@@ -1,1 +1,7 @@
-console.log("hello")
+$(document).ready(function () {
+    var nodes = $(".change")
+    nodes.each(function() {
+    	if ($(this).text().substr(0,1)==='-') $(this).addClass('greenFont') 
+    		else $(this).addClass('redFont') 
+    })
+});
