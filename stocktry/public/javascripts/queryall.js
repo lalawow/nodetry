34d54856ba90d474190f40d94eb60c1f1fc36e40
lalawow <lalawow@gmail.com>
2016-01-07 20:16:15 +0800

    change a little on display

--- stocktry/public/javascripts/queryall.js
+++ stocktry/public/javascripts/queryall.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
-    var nodes = $(".change")
+	pricecolor(".change")
+	pricecolor(".dev")
+});
+
+var pricecolor = function(price_class) {
+	var nodes = $(price_class)
     nodes.each(function() {
     	if ($(this).text().substr(0,1)==='-') $(this).addClass('greenFont') 
     		else $(this).addClass('redFont') 
     })
-});
+}
